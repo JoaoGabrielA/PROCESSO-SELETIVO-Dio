778b22de1de7126d9630f041a2e315b730368192
joao <alves34joao@gmail.com>
2024-05-14 17:43:06 -0300

    nome corrijido

---
+++ src/Candidatura/ProcessoSeletivo.java.java
@@ -0,0 +1,45 @@
+package Candidatura;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+class ProcessoSeletivo {
+    public static void main(String[] args) {
+  
+    }
+
+    static void selecaoCandidatos(){
+        String [] candidatos = {"Felipe","Marcia","JUlia","PAulo","Augusto","Monica"};
+        
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase=2000.0;
+        while(candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println(" O Candidato " + candidato + " Solicitou este valor de salario ");
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado ");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido){
+            System.out.println("LIgar para o candidato");
+    }   else if (salarioBase == salarioPretendido)
+            System.out.println("contra proposta");
+        else {
+            System.out.println("Aguardando demais candidatos");
+        }
+    }
+    
+}
