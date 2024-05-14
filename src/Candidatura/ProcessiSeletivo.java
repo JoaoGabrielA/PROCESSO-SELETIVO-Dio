d12d84970dad66307768fdb042dfc853ff95a26a
joao <alves34joao@gmail.com>
2024-05-14 17:26:08 -0300

    adicionado novas linhas no codigo (candidatos)

--- src/Candidatura/ProcessiSeletivo.java
+++ src/Candidatura/ProcessiSeletivo.java
@@ -1,12 +1,37 @@
 package Candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 class ProcessoSeletivo {
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
-        
+  
     }
+
+    static void selecaoCandidatos(){
+        String [] candidatos = {"Felipe","Marcia","JUlia","PAulo","Augusto","Monica"};
+        
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase=2000.0;
+        while(candidatosSelecionados < 5) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println(" O Candidato " + candidato + " Solicitou este valor de salario ");
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado ");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+
+    }
+
+    private static double valorPretendido() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'valorPretendido'");
+    }
+
     static void analisarCandidato(double salarioPretendido) {
         double salarioBase = 2000.0;
         if (salarioBase > salarioPretendido){
