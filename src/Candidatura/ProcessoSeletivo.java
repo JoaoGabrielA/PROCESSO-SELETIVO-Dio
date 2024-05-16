605d3adc1b171aa6d6b2d853d9290800f92b8479
joao <alves34joao@gmail.com>
2024-05-16 08:20:08 -0300

    adicionado tentativa de contato

--- src/Candidatura/ProcessoSeletivo.java
+++ src/Candidatura/ProcessoSeletivo.java
@@ -1,11 +1,40 @@
 package Candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         selecaoCandidatos();
+        String[] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto", "Monica"};
+        for(String candidato: candidatos) {
+            entrandoEmContato(candidato);
+        }
     }
+
+    static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTenttando = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuarTenttando = !atendeu;
+            if(continuarTenttando)
+                tentativasRealizadas++;
+            else
+                System.out.println("Contato realizado!");
+        } while(continuarTenttando && tentativasRealizadas<3);
+
+        if (atendeu)
+            System.out.println("conseguimos contato " + candidato + " na " + tentativasRealizadas);
+        else 
+            System.out.println("Não conseguimos contato " + candidato + " na " + tentativasRealizadas);
+    }
+
+    static boolean atender() {
+        return new Random().nextInt(3)==1;
+    }
+
 
     static void imprimirSelecionados() {
         String[] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto", "Monica"};
